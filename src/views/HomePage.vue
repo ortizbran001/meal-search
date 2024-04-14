9f0c8390145dd55d2374018e33489c322a99670f
ortizbran001 <ortizbran001@hotmail.com>
2024-04-13 23:27:19 -0500

    Fixed HomePage Title/Name

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -17,7 +17,7 @@ onMounted(async () => {
 
 <template>
  <div class="p-8 pb-0 text-orange-500">
-  <h1 class="text-4xl font-bold mb-4"></h1>
+  <h1 class="text-4xl font-bold mb-4"> Random Meals </h1>
  </div>
  <Meals :meals="meals"></Meals>
 </template>
